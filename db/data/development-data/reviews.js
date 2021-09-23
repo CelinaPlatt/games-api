d455190b5140a6ea6d40dba59c42ee4cf77ce6a4
CelinaPlatt <alvarezdiaz.celina@gmail.com>
2021-09-23 17:40:30 +0100

    Delete added social deduction review from develpment db

--- db/data/development-data/reviews.js
+++ db/data/development-data/reviews.js
@@ -273,28 +273,5 @@ module.exports = [
     category: 'push-your-luck',
     created_at: new Date(1610964545410),
     votes: 18,
-  },
-  {
-    title: 'Escape The Dark Sector',
-    designer: 'Alex Crispin,',
-    owner: 'jessjelly',
-    review_img_url:
-      'https://images.pexels.com/photos/596132/pexels-photo-596132.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    review_body:
-      "Argueably one of the best things to come out of 2020, and new addition to what will hopefully become more than a two part series. Escape the Dark Sector takes everything about Escape the Dark Castle that made it wonderful, and then adds guns! Thrust into the future by the imersive chapter cards, your mission remains the same, (thats right, escape!) but the challenges you'll face feel a whole lot more grown up. Cooperative and atmospheric, Escape the Dark Sector brings more variety and a bigger challenge to players. Don't underestimate the dabger of running out of ammo.",
-    category: 'push-your-luck',
-    created_at: new Date(1610964545610),
-    votes: 11,
-  },
-  {
-    title: 'Ultimate Werewolf',
-    designer: 'Akihisa Okui',
-    owner: 'bainesface',
-    review_img_url:
-      'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
-    review_body: "We couldn't find the werewolf!",
-    category: 'social deduction',
-    created_at: new Date(1610964101251),
-    votes: 5,
-  },
+  }
 ];
