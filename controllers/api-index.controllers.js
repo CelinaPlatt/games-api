98d78fafe7b6170d725b87e7bd486adf46d48570
CelinaPlatt <alvarezdiaz.celina@gmail.com>
2021-11-15 14:31:16 +0000

    Update Json Endpoint directory

--- controllers/api-index.controllers.js
+++ controllers/api-index.controllers.js
@@ -3,7 +3,11 @@ const { readEndpointsJson } = require('../models/api-index.models.js');
 exports.getApiEndpoints = async (req, res, next) => {
   try {
     const endpointsIndexData = await readEndpointsJson();
-    res.status(200).send({ msg: "Deployed with CI/CD", endpoints: endpointsIndexData });
+    res.status(200).send({
+      'Welcome to Game Critic API!':
+        'Please find below the available end points with their corresponding request and response example',
+      endpoints: endpointsIndexData,
+    });
   } catch (err) {
     next(err);
   }
